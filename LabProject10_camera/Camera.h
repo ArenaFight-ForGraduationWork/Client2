ba142da699bae184874a0428815b70e79915fccf
leverie <leverie623+GitHub@gmail.com>
2016-02-22 02:50:29 +0900

    Revert실험
    ㅇㅇ

--- LabProject10_camera/Camera.h
+++ LabProject10_camera/Camera.h
@@ -8,11 +8,16 @@
 #define FRAME_BUFFER_HEIGHT	480
 #define ASPECT_RATIO	(float(FRAME_BUFFER_WIDTH)/float(FRAME_BUFFER_HEIGHT))
 
+
+
+
+
 struct VS_CB_VIEWPROJECTION_MATRIX
 {
 	D3DXMATRIX m_d3dxmtxView;
 	D3DXMATRIX m_d3dxmtxProjection;
 };
+
 
 
 class CCamera
@@ -37,8 +42,8 @@ public:
 	D3DXMATRIX GetProjectionMatrix() { return(m_d3dxmtxProjection); }
 	ID3D11Buffer *GetCameraConstantBuffer() { return(m_pd3dcbCamera); }
 
-	void SetPosition(D3DXVECTOR3 d3dxvPosition) { m_d3dxvPosition = d3dxvPosition; }
-	D3DXVECTOR3& GetPosition() { return(m_d3dxvPosition); }
+	void SetPosition(D3DXVECTOR3 d3dxvPosition);
+	D3DXVECTOR3& GetPosition();
 
 	void SetLookAtPosition(D3DXVECTOR3 d3dxvLookAtWorld) { m_d3dxvLookAtWorld = d3dxvLookAtWorld; }
 	D3DXVECTOR3& GetLookAtPosition() { return(m_d3dxvLookAtWorld); }
@@ -51,14 +56,14 @@ public:
 	float& GetRoll() { return(m_fRoll); }
 	float& GetYaw() { return(m_fYaw); }
 
-	void SetOffset(D3DXVECTOR3 d3dxvOffset) { m_d3dxvOffset = d3dxvOffset; m_d3dxvPosition += d3dxvOffset; }
+	void SetOffset(D3DXVECTOR3 d3dxvOffset) { m_d3dxvOffset = d3dxvOffset; SetPosition(GetPosition() + d3dxvOffset); }
 	D3DXVECTOR3& GetOffset() { return(m_d3dxvOffset); }
 
 	void SetTimeLag(float fTimeLag) { m_fTimeLag = fTimeLag; }
 	float GetTimeLag() { return(m_fTimeLag); }
 
 	//카메라를 d3dxvShift 만큼 이동하는 가상함수이다.
-	virtual void Move(const D3DXVECTOR3& d3dxvShift) { m_d3dxvPosition += d3dxvShift; }
+	virtual void Move(const D3DXVECTOR3& d3dxvShift) { SetPosition(GetPosition() + d3dxvShift); }
 	//카메라를 x-축, y-축, z-축으로 회전하는 가상함수이다.
 	virtual void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f) { }
 	//카메라의 이동, 회전에 따라 카메라의 정보를 갱신하는 가상함수이다.
@@ -67,8 +72,8 @@ public:
 	virtual void SetLookAt(D3DXVECTOR3& vLookAt) { }
 
 protected:
-	//카메라의 위치(월드좌표계) 벡터이다.
-	D3DXVECTOR3 m_d3dxvPosition;
+	////카메라의 위치(월드좌표계) 벡터이다.
+	//D3DXVECTOR3 m_d3dxvPosition;
 	// 카메라의 로컬 x - 축(Right), y - 축(Up), z - 축(Look)을 나타내는 벡터이다.
 	D3DXVECTOR3 m_d3dxvRight;
 	D3DXVECTOR3 m_d3dxvUp;
@@ -118,7 +123,6 @@ class CThirdPersonCamera : public CCamera
 public:
 	CThirdPersonCamera();
 
-	//virtual void Update(float fTimeScale);
 	virtual void Update(const D3DXVECTOR3 *pd3dxvPosition, float fTimeElapsed);
 	virtual void SetLookAt(D3DXVECTOR3& vLookAt);
 };
