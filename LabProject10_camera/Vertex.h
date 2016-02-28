19b817eeb8674b3f9714e40b222c2d954fe74f51
leverie <leverie623+GitHub@gmail.com>
2016-02-28 20:24:51 +0900

    Vertex파일 빼기
    다음은 버텍스클래스 변경

---
+++ LabProject10_camera/Vertex.h
@@ -0,0 +1,63 @@
+#ifndef VERTEX_H_
+#define VERTEX_H_
+
+
+
+class CDiffusedVertex
+{
+public:
+	CDiffusedVertex(float x, float y, float z, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex(D3DXVECTOR3 d3dxvPosition, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = d3dxvPosition; m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxcDiffuse = D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.0f); }
+	~CDiffusedVertex() { }
+
+private:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXCOLOR m_d3dxcDiffuse;
+};
+
+class CNormalVertex
+{
+public:
+	CNormalVertex(float x, float y, float z, float nx, float ny, float nz) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxvNormal = D3DXVECTOR3(nx, ny, nz); }
+	CNormalVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR3 d3dxvNormal) { m_d3dxvPosition = d3dxvPosition; m_d3dxvNormal = d3dxvNormal; }
+	CNormalVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvNormal = D3DXVECTOR3(0.0f, 0.0f, 0.0f); }
+	~CNormalVertex() { }
+
+	void SetNormal(D3DXVECTOR3 d3dxvNormal) { m_d3dxvNormal = d3dxvNormal; }
+
+private:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR3 m_d3dxvNormal;
+};
+
+class CTexturedVertex
+{
+public:
+	CTexturedVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvTexCoord = D3DXVECTOR2(0.0f, 0.0f); }
+	CTexturedVertex(float x, float y, float z, float u, float v) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxvTexCoord = D3DXVECTOR2(u, v); }
+	CTexturedVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR2 d3dxvTexture) { m_d3dxvPosition = d3dxvPosition; m_d3dxvTexCoord = d3dxvTexture; }
+	~CTexturedVertex() { }
+
+private:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR2 m_d3dxvTexCoord;
+};
+
+class CTexturedNormalVertex
+{
+public:
+	CTexturedNormalVertex(float x, float y, float z, float nx, float ny, float nz, float u, float v) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxvNormal = D3DXVECTOR3(nx, ny, nz); m_d3dxvTexCoord = D3DXVECTOR2(u, v); }
+	CTexturedNormalVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR3 d3dxvNormal, D3DXVECTOR2 d3dxvTexCoord) { m_d3dxvPosition = d3dxvPosition; m_d3dxvNormal = d3dxvNormal; m_d3dxvTexCoord = d3dxvTexCoord; }
+	CTexturedNormalVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvNormal = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvTexCoord = D3DXVECTOR2(0.0f, 0.0f); }
+	~CTexturedNormalVertex() { }
+
+private:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR3 m_d3dxvNormal;
+	D3DXVECTOR2 m_d3dxvTexCoord;
+};
+
+
+
+#endif
